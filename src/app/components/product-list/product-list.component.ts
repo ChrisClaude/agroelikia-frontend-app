0ee3b385df427f735f67c9f40dcbbe4447327feb
Chris Claude <christ.tchambila@gmail.com>
2021-12-11 21:03:27 +0200

    added product placeholder loader

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -9,12 +9,15 @@ import {ProductService} from "@/services/product.service";
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  isProductsLoading: boolean = true;
+  placeholderArray = Array(7).fill(0);
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(products => {
       console.log(products);
+      this.isProductsLoading = false;
       this.products = products;
     });
   }
