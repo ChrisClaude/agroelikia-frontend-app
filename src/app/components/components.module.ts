8235d51f420d893558b7f10ba0dfb806abff8754
Chris Claude <christ.tchambila@gmail.com>
2021-12-23 20:18:45 +0200

    updated product creation for mobile users

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,9 @@
 import {NgModule} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CommonModule} from '@angular/common';
 import {LayoutComponent} from "@/components/layout/layout.component";
 import {HeaderComponent} from "@/components/header/header.component";
 import {MaterialModule} from "../material/material.module";
 import {RouterModule} from '@angular/router';
-import {BrowserModule} from "@angular/platform-browser";
 import {SidenavComponent} from "@/components/sidenav/sidenav.component";
 
 const components = [LayoutComponent, HeaderComponent, SidenavComponent];
@@ -13,8 +11,6 @@ const components = [LayoutComponent, HeaderComponent, SidenavComponent];
 @NgModule({
   declarations: components,
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     CommonModule,
     MaterialModule,
     RouterModule
--- src/app/pages/products/products-routing.module.ts
+++ src/app/pages/products/products-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     component: CreateProductComponent
   },
   {
-    path: 'edit',
+    path: 'edit/:id',
     component: EditProductComponent
   },
 ];
