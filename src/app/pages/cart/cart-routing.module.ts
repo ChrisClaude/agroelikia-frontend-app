28c38297da0a2333e45c76b68cddac4fbc2fb59b
Claude De-Tchambila <christ.tchambila@gmail.com>
2022-07-31 01:19:39 +0200

    Updated format

--- src/app/pages/cart/cart-routing.module.ts
+++ src/app/pages/cart/cart-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ViewComponent} from "./view/view.component";
+import { ViewComponent } from "./view/view.component";
 import { OrderFormComponent } from "@/pages/cart/orders/order-form.component";
 import { OrderConfirmationComponent } from "@/pages/cart/orders/order-confirmation/order-confirmation.component";
+import { OrderPaymentComponent } from "@/pages/cart/order-payment/order-payment.component";
 
 const routes: Routes = [
-  { path: '', component: ViewComponent },
-  { path: 'orders/form', component: OrderFormComponent },
-  { path: 'orders/success', component: OrderConfirmationComponent },
+  {path: '', component: ViewComponent},
+  {path: 'orders/form', component: OrderFormComponent},
+  {path: 'orders/payment', component: OrderPaymentComponent},
+  {path: 'orders/success', component: OrderConfirmationComponent},
 ];
 
 @NgModule({
--- src/app/pages/cart/cart.module.ts
+++ src/app/pages/cart/cart.module.ts
@@ -7,7 +7,8 @@ import {ComponentsModule} from "@/components/components.module";
 import {MaterialModule} from 'app/material/material.module';
 import { OrderFormComponent } from '@/pages/cart/orders/order-form.component';
 import { OrderConfirmationComponent } from "./orders/order-confirmation/order-confirmation.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
+import {OrderPaymentComponent} from './order-payment/order-payment.component';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { ReactiveFormsModule } from "@angular/forms";
     ViewComponent,
     OrderFormComponent,
     OrderConfirmationComponent,
+    OrderPaymentComponent,
   ],
   imports: [
     CommonModule,
